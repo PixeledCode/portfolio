356805a1fd43e1a75ccc9aba7ee2178ddef7727d
PixeledCode <ashoaib540918@gmail.com>
2024-03-23 14:46:11 +0530

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,58 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['var(--font-geist-sans)'],
+				mono: ['var(--font-geist-mono)'],
+			},
+			colors: {
+				white: 'var(--white)',
+				black: 'var(--black)',
+				selection: 'var(--selection)',
+				bg: 'var(--bg)',
+				hover: 'var(--hover)',
+				default: 'var(--text-color)',
+				muted: 'var(--text-muted)',
+				alpha: 'var(--alpha)',
+				'shadow-color': 'var(--shadow-color)',
+				accent: 'var(--accent)',
+			},
+			spacing: {
+				page: '1072px',
+				content: '640px',
+				top: 'var(--page-top)',
+
+				0: '0',
+				'025': '1px',
+				'05': '2px',
+				'1': '4px',
+				'2': '8px',
+				'3': '12px',
+				'4': '16px',
+				'5': '20px',
+				'6': '24px',
+				'7': '28px',
+				'8': '32px',
+				'9': '36px',
+				'10': '40px',
+				'11': '44px',
+				'12': '48px',
+				'14': '56px',
+				'16': '64px',
+				'20': '80px',
+				'24': '96px',
+				'28': '112px',
+				'32': '128px',
+			},
+		},
+	},
+	plugins: [],
+}
+export default config
