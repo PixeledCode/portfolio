a38acae1ab1e0f40a1860bc7ebb08e34f875f60a
pixeledcode <ashoaib540918@gmail.com>
2024-03-23 17:42:58 +0530

    add blur

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,51 @@
-import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import { cookies } from 'next/headers'
 
 import './globals.css'
 import { ThemeToggle } from '@/components/theme-toggle'
+import { siteConfig } from '@/config/site'
 
-export const metadata: Metadata = {
-	title: 'PixeledCode',
-	description: 'Frontend Engineer, building delightful web experiences.',
+export async function generateMetadata() {
+	return {
+		metadataBase: new URL(siteConfig.url),
+		title: {
+			default: siteConfig.name,
+			template: `%s | ${siteConfig.name}`,
+		},
+		description: siteConfig.description,
+		keywords: [
+			'Next.js',
+			'React',
+			'Server Components',
+			'Radix UI',
+			'OPub',
+			'Open Publishing',
+		],
+		creator: 'PixeledCode',
+		openGraph: {
+			type: 'website',
+			locale: 'en_US',
+			url: siteConfig.url,
+			title: siteConfig.name,
+			description: siteConfig.description,
+			siteName: siteConfig.name,
+			images: [`${siteConfig.url}/og.png`],
+		},
+		twitter: {
+			card: 'summary_large_image',
+			title: siteConfig.name,
+			description: siteConfig.description,
+			images: [`${siteConfig.url}/og.png`],
+			creator: 'PixeledCode',
+		},
+		icons: {
+			icon: '/favicon.ico',
+			shortcut: '/favicon-16x16.png',
+			apple: `${siteConfig.url}/apple-touch-icon.png`,
+		},
+		manifest: `${siteConfig.url}/site.webmanifest`,
+	}
 }
 
 export default function RootLayout({
@@ -25,9 +62,8 @@ export default function RootLayout({
 				<ThemeScript />
 			</head>
 			<body className={`${GeistSans.variable} ${GeistMono.variable}`}>
-				<header>
-					<ThemeToggle initialTheme={theme} />
-				</header>
+				<ThemeToggle initialTheme={theme} />
+				<div className="blur"></div>
 				{children}
 			</body>
 		</html>
---
+++ config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+	name: 'PixeledCode',
+	description: 'Frontend Engineer, building delightful web experiences.',
+	url: 'https://pixeledcode.com',
+}
