7bb700fe56730028f952a638f543b15a40cff747
pixeledcode <ashoaib540918@gmail.com>
2024-03-24 13:00:45 +0530

    update dark mode grays

--- app/talk/echarts-satori/components/Layout.tsx
+++ app/talk/echarts-satori/components/Layout.tsx
@@ -8,7 +8,7 @@ export const Layout = ({
 	description: string | React.ReactNode
 }) => {
 	return (
-		<section className="pt-12 md:pt-16">
+		<section className="pt-12 md:pt-16 vertical">
 			<h2 className="md:text-heading4 text-extra-light uppercase">{heading}</h2>
 
 			<p className="mt-4 md:text-body1 text-medium md:min-h-20 max-w-[834px]">
--- app/talk/echarts-satori/pages/page-10.tsx
+++ app/talk/echarts-satori/pages/page-10.tsx
@@ -7,48 +7,49 @@ import { Layout } from '../components/Layout'
 export const Page10 = () => {
 	return (
 		<Layout heading="fin." description="That’s all for today.">
-			<div className="md:text-body2">
-				<p className="text-light">Code & Demo</p>
-				<a
-					className="text-accent"
-					target="_blank"
-					rel="noreferrer"
-					href="https://github.com/PixeledCode/echarts-satori"
-				>
-					github.com/PixeledCode/echarts-satori
-				</a>
-			</div>
+			<div className="flex-grow vertical justify-between">
+				<div className="md:text-body2">
+					<p className="text-light">Code & Demo</p>
+					<a
+						className="text-accent"
+						target="_blank"
+						rel="noreferrer"
+						href="https://github.com/PixeledCode/echarts-satori"
+					>
+						github.com/PixeledCode/echarts-satori
+					</a>
+				</div>
 
-			<div className="w-60 md:w-[360px] aspect-square bg-gray-300 mx-auto mt-10 md:mt-20"></div>
-			<div className="mt-14 mx-auto horizontal center flex-wrap gap-4 md:text-body2 pb-10">
-				<a
-					href="https://linkedin.com/in/pixeledcode"
-					className="flex items-center gap-1"
-				>
-					<LinkedIn />
-					<span>/in/pixeledcode</span>
-				</a>
-				<a
-					href="https://twitter.com/pixeledcode"
-					className="flex items-center gap-1"
-				>
-					<Twitter />
-					<span>/pixeledcode</span>
-				</a>
-				<a
-					href="https://codepen.io/pixeledcode"
-					className="flex items-center gap-1"
-				>
-					<Codepen />
-					<span>/pixeledcode</span>
-				</a>
-				<a
-					href="https://github.com/pixeledcode"
-					className="flex items-center gap-1"
-				>
-					<Github />
-					<span>/pixeledcode</span>
-				</a>
+				<div className="mt-14 mx-auto horizontal center flex-wrap gap-4 md:text-body2 pb-10">
+					<a
+						href="https://linkedin.com/in/pixeledcode"
+						className="flex items-center gap-1"
+					>
+						<LinkedIn />
+						<span>/in/pixeledcode</span>
+					</a>
+					<a
+						href="https://twitter.com/pixeledcode"
+						className="flex items-center gap-1"
+					>
+						<Twitter />
+						<span>/pixeledcode</span>
+					</a>
+					<a
+						href="https://codepen.io/pixeledcode"
+						className="flex items-center gap-1"
+					>
+						<Codepen />
+						<span>/pixeledcode</span>
+					</a>
+					<a
+						href="https://github.com/pixeledcode"
+						className="flex items-center gap-1"
+					>
+						<Github />
+						<span>/pixeledcode</span>
+					</a>
+				</div>
 			</div>
 		</Layout>
 	)
