be495692c3d6317f8ac633482dea9f7fc824d8e4
PixeledCode <ashoaib540918@gmail.com>
2024-03-29 23:24:29 +0530

    update page 3

--- app/talk/echarts-satori/pages/page-3.tsx
+++ app/talk/echarts-satori/pages/page-3.tsx
@@ -8,21 +8,26 @@ export const Page3 = () => {
 			description="I work with dashboards on a daily basis. A common ask is having charts
 		that need to be shareable."
 		>
-			<div className="w-full flex gap-3 md:h-[573px] mt-8 md:mt-12 overflow-auto md:overflow-visible">
-				<Image
-					src="/deck/d4d.png"
-					alt="screenshot of d4d platform"
-					width={554}
-					height={355}
-					className="object-contain max-w-full"
-				/>
-				<Image
-					src="/deck/constituency.png"
-					alt="screenshot of constituency platform"
-					width={554}
-					height={355}
-					className="object-contain max-w-full"
-				/>
+			<div className="w-full md:h-[573px] mt-8 md:mt-12 overflow-auto md:overflow-visible">
+				<div className="flex gap-3 ">
+					<Image
+						src="/deck/d4d.png"
+						alt="screenshot of d4d platform"
+						width={554}
+						height={355}
+						className="object-contain max-w-full"
+					/>
+					<Image
+						src="/deck/constituency.png"
+						alt="screenshot of constituency platform"
+						width={554}
+						height={355}
+						className="object-contain max-w-full"
+					/>
+				</div>
+				<p className="mt-6 text-center text-extra-light">
+					some of the dashboards I’ve made in the past
+				</p>
 			</div>
 		</Layout>
 	)
